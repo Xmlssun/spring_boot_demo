b7e4cbdf66b444f194e8957f46a2d74eee687f50
Wangyaohui <xmlssun@gmail.com>
2019-06-02 11:27:34 +0800

    complete the first version

--- springboot/src/main/java/org/newit/springboot/demo/Application.java
+++ springboot/src/main/java/org/newit/springboot/demo/Application.java
@@ -1,13 +1,27 @@
 package org.newit.springboot.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 @Controller
 public class Application {
+    @Value("${newit.user.name}")
+    private String myName;
+
+    @Autowired
+    private Books book;
+
+    @RequestMapping("/book")
+    @ResponseBody
+    public Books getBook() {
+        return book;
+    }
 
     @RequestMapping("/hello")
     @ResponseBody
@@ -18,5 +32,20 @@ public class Application {
     @RequestMapping("/index")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/myname")
+    @ResponseBody
+    public String myName() {
+        return myName;
+    }
+
+
+    @Value("${org.newit.environment}")
+    private String environment;
+    @RequestMapping("/environment")
+    @ResponseBody
+    public String getEnvironment(){
+        return environment;
     }
 }
--- springboot/src/main/java/org/newit/springboot/demo/Books.java
+++ springboot/src/main/java/org/newit/springboot/demo/Books.java
@@ -1,4 +1,37 @@
 package org.newit.springboot.demo;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "newit.book")
 public class Books {
+
+    private String name;
+    private int price;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
