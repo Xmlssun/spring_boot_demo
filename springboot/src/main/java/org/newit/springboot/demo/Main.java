5c769202b918be8dc7693a85c487885bf7a9603d
Wangyaohui <xmlssun@gmail.com>
2019-06-02 09:23:45 +0800

    add springboot demo

---
+++ springboot/src/main/java/org/newit/springboot/demo/Main.java
@@ -0,0 +1,10 @@
+package org.newit.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
